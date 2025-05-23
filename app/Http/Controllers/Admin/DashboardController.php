eef3ec775d3eb691dd83fbef1161951542f03f49
winter <annisaallyasalsabillahh@gmail.com>
2025-05-23 13:39:05 +0700

    Add dashboardController

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Comic;
+use App\Models\Chapter;
+use App\Models\Pengumuman;
+use App\Models\Comment;
+use App\Models\Upvote;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        return view('admin.dashboard.index', [
+            'totalComics' => Comic::count(),
+            'totalChapters' => Chapter::count(),
+            'totalPengumuman' => Pengumuman::count(),
+            'totalComment' => Comment::count(),
+            'topUpvoteComics' => Comic::withCount('upvotes')->orderBy('upvotes_count', 'desc')->take(5)->get()
+        ]);
+    }
+}
